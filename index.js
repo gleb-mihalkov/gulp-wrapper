7c6af0a2c4623769e6f5dd86dbeb9efbd62d312c
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-06-21 17:22:45 +0300

    Reorganize the file's tree.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const gulp = require('gulp');
-const configMixin = require('./config.js');
-const taskMixin = require('./task.js');
-const watchMixin = require('./watch.js');
+const configMixin = require('./mixins/config.js');
+const taskMixin = require('./mixins/task.js');
+const watchMixin = require('./mixins/watch.js');
 
 module.exports = function() {
 	configMixin(gulp);
