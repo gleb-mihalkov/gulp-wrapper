8bb6de498aeca7e79814682568ce19b4ad7e9ede
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-06-21 14:49:41 +0300

    Add gulp.config().

--- config.js
+++ config.js
@@ -1,5 +1,43 @@
 'use strict';
 
+const Path = require('path');
+const cwd = process.cwd();
+
+function resolve(value, def) {
+  def = def || '.';
+  return Path.resolve(def, value);
+}
+
+let defaults = {
+  srcPath: cwd,
+  dstPath: cwd
+};
+
+let filters = {
+  srcPath: resolve,
+  dstPath: resolve
+};
+
+function merge(dst, src) {
+  for (let key in src) {
+    if (!src.hasOwnProperty(key)) continue;
+
+    let value = src[key];
+    let filter = filters[key];
+
+    if (filter) {
+      value = filter(value, dst[key]);
+    }
+
+    dst[key] = value;
+  }
+}
+
 module.exports = function(gulp) {
 
+  gulp.config = function(options) {
+    merge(gulp.config, options);
+  };
+
+  merge(gulp.config, defaults);
 };
