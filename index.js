ffb5bc0320122e601c30b1ec629d10a6f4883abb
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-06-21 12:29:12 +0300

    gulp.task - in process.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 'use strict'
 
 const gulp = require('gulp');
+
+module.exports = function() {
+
+	let taskFn = gulp.task;
+
+	gulp.task = function() {
+		let args = Array.prototype.slice.call(arguments, 0);
+		let argsCount = args.length;
+
+		if (argsCount < 2) {
+			args.push(null);
+		}
+
+		if (argsCount < 3) {
+			args.splice(1, 0, null);
+		}
+
+		let task = args[0];
+		let deps = args[1];
+		let fn = args[2];
+
+		console.log(args);
+	};
+
+	return gulp;
+}();
