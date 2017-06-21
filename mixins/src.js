4ab6f277e8fff4152f91b0be9047761554881a73
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-06-22 00:03:53 +0300

    Add gulp.src() function - preview.

--- mixins/src.js
+++ mixins/src.js
@@ -1,5 +1,21 @@
 'use strict';
 
-module.exports = function(gulp) {
+const errorHandler = require('../units/error_handler.js');
+const globResolve = require('../utils/glob_resolve.js');
+const plumber = require('gulp-plumber');
 
+let handler = errorHandler();
+
+module.exports = function(gulp) {
+  gulp._srcFn = gulp.src;
+
+  gulp.src = function(src, opts) {
+    src = globResolve(gulp.config.src, src);
+    opts = opts || {};
+
+    return gulp._srcFn(src, opts)
+      .pipe(plumber({
+        errorHandler: handler
+      }));
+  };
 };
