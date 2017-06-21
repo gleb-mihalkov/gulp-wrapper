514db7b0558655155dcbb0766bb021813b4373d7
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-06-21 17:21:20 +0300

    Add gulp.watch() - in process.

--- watch.js
+++ watch.js
@@ -1,5 +1,45 @@
 'use strict';
 
+const watch = require('gulp-watch');
+const globResolve = require('./glob-resolve.js');
+
+function getCallback(gulp, tasks) {
+  tasks = tasks || [];
+
+  if (!(tasks instanceof Array)) {
+    tasks = [tasks];
+  }
+
+  return function() {
+    for (let i = 0; i < tasks.length; i++) {
+      let task = tasks[i];
+      gulp.start(task);
+    }
+  };
+}
+
 module.exports = function(gulp) {
   
+  gulp.watch = function() {
+    let args = Array.prototype.slice.call(arguments, 0);
+
+    if (args.length < 2) {
+      args.push(null);
+    }
+
+    if (args.length < 3) {
+      args.splice(1, 0, null);
+    }
+
+    let glob = args[0];
+    let opts = args[1];
+    let cb = args[2];
+
+    if (typeof(cb) !== 'function') {
+      cb = getCallback(cb);
+    }
+
+    glob = globResolve(gulp.config.src, glob);
+    watch(glob, opts, cb);
+  };
 };
