d91ae4b04c9a59db62430f259b6f679f36bba1d4
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-06-21 23:58:54 +0300

    Add the gulp.src() files.

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const gulp = require('gulp');
 const configMixin = require('./mixins/config.js');
 const taskMixin = require('./mixins/task.js');
 const watchMixin = require('./mixins/watch.js');
+const srcMixin = require('./mixins/src.js');
 
 module.exports = function() {
 	configMixin(gulp);
 	taskMixin(gulp);
   watchMixin(gulp);
+  srcMixin(gulp);
 
 	return gulp;
 }();
