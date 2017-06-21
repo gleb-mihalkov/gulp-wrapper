b8c741fccd4bad80001c7dfc3f639171781b5900
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-06-21 14:29:41 +0300

    Add file for gulp.config().

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const gulp = require('gulp');
+const configMixin = require('./config.js');
 const taskMixin = require('./task.js');
 
 module.exports = function() {
+	configMixin(gulp);
 	taskMixin(gulp);
 
 	return gulp;
