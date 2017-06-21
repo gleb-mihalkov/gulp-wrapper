bac578e0194d84a9aaddebc60c74e9fb99fb26f6
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-06-21 17:30:51 +0300

    Add globResolve() function - preview.

--- utils/glob_resolve.js
+++ utils/glob_resolve.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const Path = require('path');
+
+function resolve(base, path) {
+  path = Path.resolve(base, path);
+  let cwd = process.cwd();
+
+  return path.indexOf(cwd) === 0
+    ? '.' + path.substr(cwd.length)
+    : path;
+}
+
+module.exports = function(base, glob) {
+  if (glob == null) return base;
+
+  if (typeof(glob) == 'string') {
+    glob = [glob];
+  }
+
+  let list = [];
+
+  for (let i = 0; i < glob.length; i++) {
+    let item = glob[i];
+
+    let isIgnore = item[0] == '!';
+    
+    if (isIgnore) {
+      item = item.substr(1);
+    }
+    
+    let prefix = isIgnore ? '!' : '';
+    let value = prefix + resolve(base, item);
+
+    list.push(value);
+  }
+
+  return list;
+};
