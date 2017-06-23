4325f054dd91338f09fcac4e19992af81a6ce74d
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-06-23 13:31:43 +0300

    gulp.dst() - in process.

--- mixins/dst.js
+++ mixins/dst.js
@@ -1,9 +1,54 @@
 'use strict';
 
+const concat = require('gulp-concat');
+const Path = require('path');
+
+function splitDest(dest) {
+  let separator = Path.sep;
+  let index = dest.lastIndexOf(separator);
+
+  let basename = null;
+  let filename = null;
+
+  if (index < 0) {
+    filename = dest;
+    basename = '.';
+  }
+  else {
+    basename = dest.substr(0, index);
+    filename = dest.substr(index + 1);
+  }
+
+  let isBasename = filename && filename.indexOf('.') < 0;
+
+  if (isBasename) {
+    basename = basename + separator + filename;
+    filename = '';
+  }
+
+  return [basename, filename];
+}
+
 module.exports = function(gulp) {
   gulp._dstFn = gulp.dest;
 
-  gulp.dst = function() {
+  gulp.dst = function(dest) {
+    dest = splitDest(dest);
 
+    let basename = dest[0];
+    let filename = dest[1];
+
+    let destStream = gulp._dstFn(basename);
+    let resultStream = destStream;
+
+    if (filename) {
+      let concatStream = concat(filename);
+      concatStream.pipe(destStream);
+      resultStream = concatStream;
+    }
+
+    return resultStream;
   };
 };
+
+module.exports._splitDest = splitDest;
