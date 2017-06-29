cfc8580bc7cab5aa77b41950ae7b5f009f0161a1
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-06-29 15:59:29 +0300

    Add comments to gulp.config() method.

--- mixins/config.js
+++ mixins/config.js
@@ -3,21 +3,41 @@
 const Path = require('path');
 const cwd = process.cwd();
 
+/**
+ * Resolve path in file system.
+ * @param  {String} value Path.
+ * @param  {String} def   Default path.
+ * @return {String}       Absolute path.
+ */
 function resolve(value, def) {
   def = def || '.';
   return Path.resolve(def, value);
 }
 
+/**
+ * Default configuration.
+ * @type {Object}
+ */
 let defaults = {
   src: cwd,
   dst: cwd
 };
 
+/**
+ * Filters for config's properties transformation.
+ * @type {Object}
+ */
 let filters = {
   src: resolve,
   dst: resolve
 };
 
+/**
+ * Merge objects using filters and default values.
+ * @param  {Object} dst Destination object.
+ * @param  {Object} src Source object.
+ * @return {void}
+ */
 function merge(dst, src) {
   for (let key in src) {
     if (!src.hasOwnProperty(key)) continue;
@@ -33,6 +53,11 @@ function merge(dst, src) {
   }
 }
 
+/**
+ * Add configuration mixin to Gulp instance.
+ * @param  {Object} gulp Gulp instance.
+ * @return {void}
+ */
 module.exports = function(gulp) {
 
   gulp.config = function(options) {
