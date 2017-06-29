05e03e436a5d107127f37a2d16b896275c40c3d5
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-06-29 16:03:04 +0300

    Add comments for gulp.is() method.

--- mixins/ignore.js
+++ mixins/ignore.js
@@ -2,6 +2,11 @@
 
 const ignore = require('gulp-ignore');
 
+/**
+ * Add ignore mixin to Gulp instance.
+ * @param  {Object} gulp Gulp instance.
+ * @return {void}
+ */
 module.exports = function(gulp) {
   gulp.ignore = ignore.exclude;
 };
--- mixins/is.js
+++ mixins/is.js
@@ -2,6 +2,11 @@
 
 const gulpif = require('gulp-if');
 
+/**
+ * Add conditional mixin to Gulp instance.
+ * @param  {Object} gulp Gulp instance.
+ * @return {void}
+ */
 module.exports = function(gulp) {
   gulp.is = gulpif;
 };
